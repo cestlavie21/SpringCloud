0d1d45382140b456ab87a2548cbde99fa0721be1
汪福成 <wangfc@anchnet.com>
2019-05-13 18:12:11 +0800

    add: 基于consul config 的配置中心

--- springcloud/consulprovider/src/main/java/com/test/consulprovider/HelloController.java
+++ springcloud/consulprovider/src/main/java/com/test/consulprovider/HelloController.java
@@ -1,17 +1,31 @@
 package com.test.consulprovider;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//开启动态刷新配置
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
     private  String port;
 
+    @Value("${foo.bar}")
+    private String bar;
+
+
     @RequestMapping("/hello")
     public String hello(){
         return "端口"+port;
     }
+
+    @RequestMapping("/bar")
+    public String getBar() {
+        return "bar:" + bar;
+    }
+
+
 }
